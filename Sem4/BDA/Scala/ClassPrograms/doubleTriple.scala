0ada013eed413fb7189563cc0b20178e8dfbf233
AghoraGuru <kalyanguru18@gmail.com>
2023-06-01 15:54:12 +0530

    Tried Scala in IntelliJ

--- Sem4/BDA/Scala/ClassPrograms/doubleTriple.scala
+++ Sem4/BDA/Scala/ClassPrograms/doubleTriple.scala
@@ -1,12 +1,26 @@
-def fno(x:Int)(y:Int) = {
-    if (x % 3 == 0){
-        y * 3
-    }
-    else{
-        y * 2
-    }
+def fno(x:Int): Int => Int ={
+    def double (y:Int):Int = y*2
+    def triple (y:Int):Int = y*3
+
+    if (x%3 ==0)
+        triple _
+    else 
+        double _
 }
 
-var a = fno(3)_
-var b = a(4)
-print(b)
+var ans1 = fno(3)
+println(ans1(2))
+
+//implementing annoymous function in the fno
+
+def an_fno(x:Int): Int => Int ={
+    if (x%3 ==0)
+        (y: Int) => y*3
+    if (x%2 ==0)
+        (y: Int) => y*2
+    else
+        (y: Int) => y*1
+}
+//calling the function
+var ans = an_fno(3)
+println(ans(2))
